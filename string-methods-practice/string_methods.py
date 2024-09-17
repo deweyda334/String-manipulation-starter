df643143e110eb35d60de39573f85d70bcbfa5b9
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-09-17 13:44:13 -0400

    Add files via upload

---
+++ string-methods-practice/string_methods.py
@@ -0,0 +1,16 @@
+# Dax Dewey Matthew hall 9/17/2024
+
+txt = "12344A297A4721"
+# txt = "its true"
+
+x = txt.replace("A", "68")
+print(x)
+
+number_of_ones = x.count("1")
+
+print(f'The string is: {x}')
+
+print(f'The number 1 appears this many times in the string: {number_of_ones}')
+
+
+
